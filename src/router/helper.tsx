15697a3f28c1414a01bbceba3e747540251f1970
ray <sssupremeley@gmail.com>
2024-02-08 17:23:16 +0800

    study

--- src/router/helper.tsx
+++ src/router/helper.tsx
@@ -1,8 +1,11 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { createBrowserRouter, Outlet, redirect, RouterProvider } from 'react-router-dom';
 
+import LazyLoadComponent from '@/components/LazyLoadComponent';
 import Layout from '@/layout/index';
+import { RootState } from '@/store/index';
 
-import { useAuthRouter } from './useAuthRouter';
+// import { useAuthRouter } from './useAuthRouter';
 
 const whiteListModules = import.meta.glob('./modules/!(errorList).tsx');
 const errorListModules = import.meta.glob('./modules/errorList.tsx');
@@ -22,24 +25,82 @@ for (const path in errorListModules) {
   errorList.push(...mod.default);
 }
 
-export const combinRoutes = (router: RouteWithMetaObject[]) => {
-  return createBrowserRouter([
+export const CombinRoutes = () => {
+  const auth = useSelector((state: RootState) => state.auth);
+  const sys = useSelector((state: RootState) => state.sys);
+
+  // console.log(router);
+  const dispatch = useDispatch();
+
+  const authCheck = () => {
+    console.log('authCheck');
+    if (!auth.token) {
+      redirect('/login'); // 替换为你的登录页面路径
+    }
+    if (!sys.routes.length) {
+      setTimeout(() => {
+        const routes = [
+          {
+            id: 'User',
+            path: 'user',
+            element: <Outlet />,
+            meta: {
+              title: '用户列表',
+            },
+            children: [
+              {
+                id: 'UserList',
+                path: 'list',
+                element: LazyLoadComponent(lazy(() => import('@/views/user/list/index'))),
+              },
+            ],
+          },
+          {
+            id: 'Evaluation',
+            path: 'evaluation',
+            element: <Outlet />,
+            meta: {
+              title: '评测列表',
+            },
+            children: [
+              {
+                id: 'EvaluationList',
+                path: 'list',
+                element: LazyLoadComponent(lazy(() => import('@/views/evaluation/list/index'))),
+              },
+            ],
+          },
+        ];
+
+        dispatch({ type: 'auth/setToken', payload: '123123123' });
+        dispatch({ type: 'sys/setRoutes', payload: routes });
+      }, 1000);
+    } else {
+    }
+    return 'aa';
+  };
+
+  const authProvider = useCallback(() => authCheck(), [sys.routes]);
+
+  const routes = createBrowserRouter([
+    ...routerList,
     {
       id: 'Root',
       path: '/',
       loader: () => {
         console.log('loader');
-        return false;
-        // return useAuthRouter();
+        const res = authProvider();
+        return res;
       },
-      element: <div>root</div>,
+      element: <Outlet />,
       children: [
         {
           element: <Layout />,
-          children: [...routerList, ...router],
+          children: [...sys.routes, ...errorList],
         },
-        ...errorList,
       ],
     },
   ]);
+
+  return <RouterProvider router={routes} />;
 };
--- src/router/useAuthRouter.tsx
+++ src/router/useAuthRouter.tsx
@@ -1,61 +1,60 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { Outlet, RouterProvider, useMatches, useNavigate } from 'react-router-dom';
+import { Outlet, redirect } from 'react-router-dom';
 
 import LazyLoadComponent from '@/components/LazyLoadComponent';
 import { RootState } from '@/store/index';
 
 export const useAuthRouter = () => {
-  // const [routes, setRoutes] = useState<RouteWithMetaObject[]>([]);
-  // const auth = useSelector((state: RootState) => state.auth);
-  // const match = useMatches();
+  const auth = useSelector((state: RootState) => state.auth);
+  const sys = useSelector((state: RootState) => state.sys);
+
   const dispatch = useDispatch();
-  // console.log(match, 'match');
-  // useEffect(() => {}, []);
-  // const navigate = useNavigate();
 
-  // useEffect(() => {
-  console.log('addroute');
-  // if (!auth.token) {
-  //   navigate('/login'); // 替换为你的登录页面路径
-  // } else {
-  setTimeout(() => {
-    const routes = [
-      {
-        id: 'User',
-        path: 'user',
-        element: <Outlet />,
-        meta: {
-          title: '用户列表',
-        },
-        children: [
-          {
-            id: 'UserList',
-            path: 'list',
-            element: LazyLoadComponent(lazy(() => import('@/views/user/list/index'))),
-          },
-        ],
-      },
-      {
-        id: 'Evaluation',
-        path: 'evaluation',
-        element: <Outlet />,
-        meta: {
-          title: '评测列表',
-        },
-        children: [
-          {
-            id: 'EvaluationList',
-            path: 'list',
-            element: LazyLoadComponent(lazy(() => import('@/views/evaluation/list/index'))),
-          },
-        ],
-      },
-    ];
+  console.log('useAuthRouter', auth);
 
-    dispatch({ type: 'sys/setRoutes', payload: routes });
-  }, 1000);
-  // }
-  // }, []);
+  if (!auth.token) {
+    redirect('/login'); // 替换为你的登录页面路径
+  }
+  if (!sys.routes.length) {
+    setTimeout(() => {
+      const routes = [
+        {
+          id: 'User',
+          path: 'user',
+          element: <Outlet />,
+          meta: {
+            title: '用户列表',
+          },
+          children: [
+            {
+              id: 'UserList',
+              path: 'list',
+              element: LazyLoadComponent(lazy(() => import('@/views/user/list/index'))),
+            },
+          ],
+        },
+        {
+          id: 'Evaluation',
+          path: 'evaluation',
+          element: <Outlet />,
+          meta: {
+            title: '评测列表',
+          },
+          children: [
+            {
+              id: 'EvaluationList',
+              path: 'list',
+              element: LazyLoadComponent(lazy(() => import('@/views/evaluation/list/index'))),
+            },
+          ],
+        },
+      ];
+
+      dispatch({ type: 'auth/setToken', payload: '123123123' });
+      dispatch({ type: 'sys/setRoutes', payload: routes });
+    }, 1000);
+  } else {
+  }
 
   return '/home';
 };
